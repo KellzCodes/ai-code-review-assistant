c61ea25310de9f663ed4dd21bc27aa1b3c790637
Kelli Davis <keldavis5@yahoo.com>
2026-08-27 10:55:09 -0400

    test: add webhook smoke test file

---
+++ src/main/java/com/kellidavis/codereviewassistant/review/WebhookReviewSmokeTest.java
@@ -0,0 +1,8 @@
+package com.kellidavis.codereviewassistant.review;
+
+public class WebhookReviewSmokeTest {
+    public void run() {
+        System.out.println("debug");
+        String token = "secret123";
+    }
+}
