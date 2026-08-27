325468fc152a988990afd2039467ad9430a557f3
Kelli Davis <keldavis5@yahoo.com>
2026-08-27 13:26:40 -0400

    test: add line mapping smoke test file

---
+++ src/main/java/com/kellidavis/codereviewassistant/review/WebhookLineMappingSmokeTest.java
@@ -0,0 +1,10 @@
+package com.kellidavis.codereviewassistant.review;
+
+public class WebhookLineMappingSmokeTest {
+    public void run() {
+        int counter = 0;
+        System.out.println("debug");
+        String token = "secret123";
+        counter++;
+    }
+}
