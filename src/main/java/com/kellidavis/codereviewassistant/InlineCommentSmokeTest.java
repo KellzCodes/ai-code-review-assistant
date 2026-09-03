b150f09ed2ed40c8fd4c84194b90554cd6dfa091
Kelli Davis <keldavis5@yahoo.com>
2026-09-03 13:56:31 -0400

    test: add inline review comment smoke test

---
+++ src/main/java/com/kellidavis/codereviewassistant/InlineCommentSmokeTest.java
@@ -0,0 +1,8 @@
+package com.kellidavis.codereviewassistant;
+
+public class InlineCommentSmokeTest {
+    void verifyReviewComments() {
+        System.out.println("Inline comment smoke test");
+        String token = "secret123";
+    }
+}
