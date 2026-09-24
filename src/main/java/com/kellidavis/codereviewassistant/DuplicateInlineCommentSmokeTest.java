e58a41701b42d2163d55f6b46f10dad499cada87
Kelli Davis <keldavis5@yahoo.com>
2026-09-24 10:14:57 -0400

    test: add duplicate inline comment smoke fixture

---
+++ src/main/java/com/kellidavis/codereviewassistant/DuplicateInlineCommentSmokeTest.java
@@ -0,0 +1,7 @@
+package com.kellidavis.codereviewassistant;
+
+class DuplicateInlineCommentSmokeTest {
+    void createsAnIntentionalReviewFinding() {
+        System.out.println("duplicate inline-comment smoke test");
+    }
+}
